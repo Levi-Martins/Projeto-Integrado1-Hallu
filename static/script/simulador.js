cf60be7d80ded9075d04884b79d253ebc13cbf9f
Daniel Cardeal <danicardeal.08@gmail.com>
2023-06-05 23:38:26 -0300

    update simulador

--- static/script/simulador.js
+++ static/script/simulador.js
@@ -1,21 +1,39 @@
 let obrigatorias;
 let eletivas;
+let optativas;
 
 async function request() {
+    // localStorage.clear()
+    const loading = document.getElementById("loading")
+    const turno = document.getElementById("turno")
     if (!localStorage.getItem('arquivosArmazenados')) {
+        turno.style.display = "none"
         console.log("Carregando dados")
         await axios.all([
             axios.get("http://127.0.0.1:8000/obrigatorias"),
-            axios.get("http://127.0.0.1:8000/eletivas")
-        ]).then(axios.spread((ob, ele) => {
+            axios.get("http://127.0.0.1:8000/eletivas"),
+            axios.get("http://127.0.0.1:8000/optativas")
+        ]).then(axios.spread((ob, ele,opt) => {
             localStorage.setItem("obrigatorias",JSON.stringify(ob.data))
             localStorage.setItem("eletivas",JSON.stringify(ele.data))
-
+            localStorage.setItem("optativas",JSON.stringify(opt.data))
         }))
         localStorage.setItem('arquivosArmazenados', true)
         console.log("Dados Carregados")
+        turno.style.display = "flex"
+        loading.style.display = "none"
+
     } else {
         console.log("Arquivos já em localStorage")
+        loading.style.display = "none"
+        
     }
 }
-request()
+
+function app(){
+    console.log("Simulação Iniciado")
+    request()
+
+}
+
+app() 
